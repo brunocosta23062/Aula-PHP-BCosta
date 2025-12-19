01087b65bfd808d7920ec6869cbc6555ec5f0583
brunocosta23062 <brunocostaaborges@gmail.com>
2025-12-18 22:30:47 -0300

    Add files via upload

---
+++ Aula-PHP-BCosta/css/js/operacoes.js
@@ -0,0 +1,24 @@
+
+
+
+let soma = 0;
+soma = 3 +5;
+soma++;
+soma+=2 
+console.log("A soma e " + soma);
+
+let sub = 0;
+sub = 20 - 2;
+sub--;
+sub-=2;
+console.log("A subtralção é: " + sub);
+
+let multi = 0;
+multi = 5 * 6;
+multi*=2;
+console.log("A multiplicação e " + multi);
+
+let div = 0;
+div  = 40 / 2;
+div/=4 ;
+console.log("a divisão é " + div);

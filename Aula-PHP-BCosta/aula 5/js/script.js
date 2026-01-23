844174e2e9c852936d7656f66ca1dafe7528275d
brunocosta23062 <brunocostaaborges@gmail.com>
2026-01-22 22:27:02 -0300

    Add files via upload

---
+++ Aula-PHP-BCosta/aula 5/js/script.js
@@ -0,0 +1,11 @@
+   
+   
+let entrada = document.getElementById("entrada")
+let saida = document.getElementById("saida")
+
+function mostrarinfo(){
+
+saida.textContent = "Você digitou: " + entrada.value;
+
+
+}
